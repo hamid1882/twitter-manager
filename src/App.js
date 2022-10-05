83760420a11a88ce374cbdd29fbc9704f4a590fd
hamid <hussainhafeez1100@gmail.com>
2022-10-05 20:21:33 +0530

    added conditionally show homepage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import Loginpage from "./components/Loginpage/Loginpage";
 import Homepage from "./components/Homepage/Homepage";
 
 function App() {
-  const queryParam = window.location.href.login
-
+  const queryParam = window.location.search.substring(1).split("=")[1]
   console.log(queryParam)
-
-
   return (
     <div>
       {
-        queryParam === undefined || queryParam === false
+        queryParam === undefined || queryParam === "false"
           ? <Loginpage />
           : <Homepage />
       }
