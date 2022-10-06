afa7dee6507ba3e38565729b84251d86029a10c9
hamid <hussainhafeez1100@gmail.com>
2022-10-06 16:04:43 +0530

    UI changes and User Details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Loginpage from "./components/Loginpage/Loginpage";
 import Homepage from "./components/Homepage/Homepage";
+import "./App.css";
 
 function App() {
   const queryParam = window.location.search.substring(1).split("=")[1]
